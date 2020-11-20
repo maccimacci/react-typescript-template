46de8755232d82e1e3c55f582c09a5f9d0ecb0f6
maQui <maximiliano.nieva@gmail.com>
2020-11-20 20:48:39 -0300

    Router reorder

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import * as React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 import SideNav from './pages/Sidebar';
 import Routes from './Routes'
-
-import './App.scss';
 import Header from './pages/Header';
 import Footer from './pages/Footer';
+
+import './App.scss';
 
 function App() {
     return(
       <div className="App">
-        <BrowserRouter>
           <Header/>
           <div className="main">
             <SideNav />
             <Routes />
           </div>
           <Footer />
-        </BrowserRouter>
       </div>
     )
 }
